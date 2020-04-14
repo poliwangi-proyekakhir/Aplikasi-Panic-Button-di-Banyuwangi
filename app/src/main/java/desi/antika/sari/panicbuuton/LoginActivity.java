92ed9aff251e380a578f724883a374c9e84a4dc8
desiantikasari <desiantikasari1999@gmail.com>
2020-04-14 11:17:54 +0700

    First Commit

---
+++ app/src/main/java/desi/antika/sari/panicbuuton/LoginActivity.java
@@ -0,0 +1,27 @@
+package desi.antika.sari.panicbuuton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        register=(TextView)findViewById(R.id.txtRegister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
